f60e8a05c3e252dca2e249f0437fbeb0db3e623a
Georgi_Varbanov <g.varbnovv@gmail.com>
2023-09-25 22:52:27 +0300

    Add constants file/set up router and controllers

--- Day4/src/index.js
+++ Day4/src/index.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const expressConfig = require("./config/expressconfig");
 const handlebarsConfig = require('./config/hbsconfig');
+const { PORT } = require('./constants');
 
 const app = express();
-const PORT = 5050;
 
 expressConfig(app);
 handlebarsConfig(app);
-
-
-
 
 app.get("/", (req, res) => {
     res.render("index");
