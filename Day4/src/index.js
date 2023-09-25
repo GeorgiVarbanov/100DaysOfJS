05eda692f584e5f44d7d4fdee0dcf39144cd62d1
Georgi_Varbanov <g.varbnovv@gmail.com>
2023-09-25 22:05:53 +0300

    Cleaning up hbs views/loaded up static views/

--- Day4/src/index.js
+++ Day4/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const path = require("path");
 const app = express();
 const PORT = 5050;
 
@@ -7,9 +8,12 @@ app.engine("hbs", handlebars.engine({extname: "hbs"}));
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 
+const staticFiles = express.static(path.resolve(__dirname,"public"));
+app.use(staticFiles)
+
 
 app.get("/", (req, res) => {
-    res.render("layouts/main");
+    res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
