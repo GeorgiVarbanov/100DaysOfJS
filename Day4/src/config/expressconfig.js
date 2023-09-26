6385ba48cf8e91ac120b1fcf488205178c2ee61e
Georgi_Varbanov <g.varbnovv@gmail.com>
2023-09-26 19:39:05 +0300

    Updating create functionality

--- Day4/src/config/expressconfig.js
+++ Day4/src/config/expressconfig.js
@@ -2,8 +2,8 @@ const express = require("express");
 const path = require("path");
 
 const expressConfig = (app) => {
-    app.use(express.static(path.resolve(__dirname,"../public")));
+  app.use(express.static(path.resolve(__dirname, "../public")));
+  app.use(express.urlencoded({ extended: false }));
 };
-
 
 module.exports = expressConfig;
--- Day4/src/public/controllers/cubeController.js
+++ Day4/src/public/controllers/cubeController.js
@@ -1,7 +1,15 @@
-const rounter = require('express').Router();
+const rounter = require("express").Router();
+const cubeService = require("../../services/cubeService");
 
 rounter.get("/create", (req, res) => {
-    res.render('create');
+  cubeService.getAll();
+  res.render("create");
+});
+
+rounter.post("/create", (req, res) => {
+  const { name, description, imageUrl, difficultyLevel } = req.body;
+  cubeService.createCube({ name, description, imageUrl, difficultyLevel: Number(difficultyLevel) });
+  res.redirect("/");
 });
 
 module.exports = rounter;
