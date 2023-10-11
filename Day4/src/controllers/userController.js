1c6c7edc0934fab1a43d5b40dd7d936f7346ec83
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-10-11 21:12:19 +0300

    Implemented logout functionality

--- Day4/src/controllers/userController.js
+++ Day4/src/controllers/userController.js
@@ -24,7 +24,10 @@ router.post("/register", async (req, res) => {
     res.redirect("/users/login");
 });
 
-
+router.get("/logout", (req, res) => {
+    res.clearCookie("auth");
+    res.redirect("/");
+});
 
 
 module.exports = router;
