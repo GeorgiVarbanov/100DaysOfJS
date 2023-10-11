c1e756808cfe88215e1a0313390c2c5ef6e52815
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-10-11 21:46:26 +0300

    Add basic password and username validation in User model

--- Day4/src/models/User.js
+++ Day4/src/models/User.js
@@ -2,8 +2,19 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
+    username: {
+        type: String,
+        required: [true, "Username is required!"],
+        minLength: [5, "Username is too short!"],
+        match: [/^[A-Za-z0-9]+$/, "Username should not containt special symbols"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        minLength: [8, "Password too short!"],
+        required: [true, "Password is required!"],
+        match: [/^[A-Za-z0-9]+$/, "Password should not containt special symbols"],
+    }
 });
 
 userSchema.virtual('repeatPassword').set(function (value) {
