81d0a6e183fda865ec47b962ded5bc2e6c77dc59
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-10-11 21:09:16 +0300

    Validate locals

--- Day4/src/middlewares/authMiddleware.js
+++ Day4/src/middlewares/authMiddleware.js
@@ -9,6 +9,9 @@ exports.auth = async (req, res, next) => {
         try {
             const decodedToken = await jwt.verify(token, SECRET);
             req.user = decodedToken;
+            res.locals.user = decodedToken;
+            res.locals.isAuthenticated = true;
+            
             next();
         } catch (error) {
             console.log({ error });
