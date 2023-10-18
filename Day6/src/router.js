7fa6b1f56b852c6a2ca36fadc002a1bb6216f7fc
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-10-18 22:56:13 +0300

    Create endpoints for login/register in userController

--- Day6/src/router.js
+++ Day6/src/router.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const homeController = require("./controllers/homeController.js");
+const homeController = require("./controller/homeController.js");
+const userController = require("./controller/userController.js");
 
 router.use(homeController);
-
+router.use("/users", userController);
 
 router.get("*", (req, res) => {
     res.render("404");
